da023611dc17a936227ba8704083df41cc6dc48b
Mason McManus <97319135+NimbleValley@users.noreply.github.com>
2023-09-18 22:25:38 -0500

    Added waypoints, play animation, max speed, and frame limiting

--- editor.js
+++ editor.js
@@ -36,6 +36,14 @@ var initialMouse = {
     y: 0
 }
 
+var points = [];
+var nextPoint = 1;
+var maxSpeed = 4;
+var thresh = 0.01;
+
+const fpsInterval = 1000 / 30;
+var then;
+
 // Window resize
 function setCanvasDimensions() {
     if (window.innerHeight > window.innerWidth) {
@@ -53,6 +61,7 @@ function setCanvasDimensions() {
     drawCanvas();
 }
 
+// Rendering loop
 function drawCanvas() {
     ctx.resetTransform();
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -62,16 +71,75 @@ function drawCanvas() {
     ctx.fillStyle = "orange";
     ctx.fillRect(ROBOT.posX * SCALE, ROBOT.posY * SCALE, ROBOT.length * SCALE, ROBOT.width * SCALE);
 
-    robotTextX.innerText = `Robot X: ${Math.round(ROBOT.posX * 0.0254 * 1000) / 1000}`;
-    robotTextY.innerText = `Robot Y: ${Math.round(ROBOT.posY * 0.0254 * 1000) / 1000}`;
+    robotTextX.innerHTML = `Robot X: <span class="regular-font">${Math.round(ROBOT.posX * 0.0254 * 1000) / 1000}</span>`;
+    robotTextY.innerHTML = `Robot Y: <span class="regular-font">${Math.round(ROBOT.posY * 0.0254 * 1000) / 1000}</span>`;
+
+    ctx.lineWidth = canvas.width * 0.005;
+    ctx.beginPath();
+    for (var i = 0; i < points.length; i++) {
+        if (i == 0) {
+            ctx.moveTo((points[i].x + ROBOT.length * 0.5) * SCALE, (points[i].y + ROBOT.width * 0.5) * SCALE);
+        } else {
+            ctx.lineTo((points[i].x + ROBOT.length * 0.5) * SCALE, (points[i].y + ROBOT.width * 0.5) * SCALE);
+        }
+        ctx.fillStyle = "#f5d742";
+    }
+    ctx.strokeStyle = "#ab9700";
+    ctx.stroke();
+    for (var i = 0; i < points.length; i++) {
+        ctx.fillRect((points[i].x + ROBOT.length * 0.5) * SCALE - canvas.width * 0.005, (points[i].y + ROBOT.width * 0.5) * SCALE - canvas.width * 0.005, canvas.width * 0.01, canvas.width * 0.01);
+    }
 }
 
+// Updating loop
 function update() {
+    drawCanvas();
 
+    let now = Date.now();
+    let elapsed = now - then;
+
+    if (elapsed > fpsInterval) {
+        then = now - (elapsed % fpsInterval);
+
+        if (nextPoint < points.length) {
+            let distX = (ROBOT.posX - points[nextPoint].x) * 0.0254;
+            let distY = (ROBOT.posY - points[nextPoint].y) * 0.0254;
+
+            let distRatio;
+            if (Math.abs((ROBOT.posX - points[nextPoint].x)) > Math.abs((ROBOT.posY - points[nextPoint].y))) {
+                distRatio = Math.abs(distY / distX);
+            } else {
+                distRatio = Math.abs(distX / distY);
+            }
+
+            if (Math.abs(distX) < thresh && Math.abs(distY) < thresh) {
+                nextPoint++;
+            } else {
+                if (distX > maxSpeed / fpsInterval)
+                    distX = maxSpeed / fpsInterval;
+                if (distY > maxSpeed / fpsInterval)
+                    distY = maxSpeed / fpsInterval;
+                if (distX < maxSpeed / fpsInterval * -1)
+                    distX = maxSpeed / fpsInterval * -1;
+                if (distY < maxSpeed / fpsInterval * -1)
+                    distY = maxSpeed / fpsInterval * -1;
+
+                if (Math.abs((ROBOT.posX - points[nextPoint].x)) > Math.abs((ROBOT.posY - points[nextPoint].y))) {
+                    distY *= distRatio;
+                } else {
+                    distX *= distRatio;
+                }
+
+                ROBOT.posX += distX / -0.0254;
+                ROBOT.posY += distY / -0.0254;
+            }
+        }
+    }
+
+    requestAnimationFrame(update);
 }
 
 function startDrag(event) {
-    //console.log(Math.abs((event.clientY) / canvas.height * 319 - (ROBOT.posY + (ROBOT.width / 2))));
 
     // Robot dragged
     if (Math.abs((window.innerWidth - event.clientX) / canvas.width * 649 - (649 - (ROBOT.posX + (ROBOT.length / 2)))) < ROBOT.length / 2 && Math.abs((event.clientY) / canvas.height * 319 - (ROBOT.posY + (ROBOT.width / 2))) < ROBOT.width / 2) {
@@ -100,8 +168,6 @@ function removeDrag() {
     document.onmousemove = "";
 }
 
-requestAnimationFrame(update);
-
 function setRobotSize() {
     ROBOT.length = prompt("What is robot length, or x-value? (inches)");
     ROBOT.width = prompt("What is robot width, or y-value? (inches)");
@@ -118,4 +184,22 @@ function setCoords() {
         ROBOT.posY = coordInputY.value / 0.0254;
         drawCanvas();
     }
+}
+
+function addPoint() {
+    console.log(ROBOT.posX);
+    var tempPoint = {
+        x: ROBOT.posX,
+        y: ROBOT.posY
+    };
+    points.push(tempPoint);
+
+    drawCanvas();
+}
+
+function playAnimation() {
+    ROBOT.posX = points[0].x;
+    ROBOT.posY = points[0].y;
+    then = Date.now();
+    update();
 }
